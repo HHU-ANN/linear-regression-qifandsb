212171d9370e125c2e7df12b0dfbab15d571738b
qifandsb <995162021@qq.com>
2023-05-01 12:57:54 +0800

    提交文件

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -9,10 +9,12 @@ except ImportError as e:
     import numpy as np
 
 def ridge(data):
-    X , y = read_data()
-    weight = np.linalg.inv(np.matmul(X.T,X)) * np.matmul(X.T, y)
-    return weight @ data
-    
+    # X , y = read_data()
+    # weight = np.linalg.inv(np.matmul(X.T,X)) * np.matmul(X.T, y)
+    # return weight @ data
+    x, y = read_data();
+    return np.dot(np.linalg.inv(np.dot(x.T, x)), np.dot(x.T, y));
+
 def lasso(data):
     x, y = read_data();
     return np.dot(np.linalg.inv(np.dot(x.T, x)) , np.dot(x.T, y));
