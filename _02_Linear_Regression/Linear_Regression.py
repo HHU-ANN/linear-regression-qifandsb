5d6e8b45a40409250586d2052c6f09fe7e0e87cb
qifandsb <995162021@qq.com>
2023-05-02 12:51:11 +0800

    sd

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -16,6 +16,8 @@ def ridge(data):
     i = np.eye(6)
     h = 3
     w = np.dot(np.linalg.inv(np.dot(x.T, x) + h * i), np.dot(x.T, y))
+    m = np.dot(x.T, y)/10 - np.dot(x.T, x)
+    print(m)
     return w @ data
 
 def lasso(data):
