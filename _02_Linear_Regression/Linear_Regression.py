03da1a3ce71b46c3ef3c6511522620c1bd61e9a3
qifandsb <995162021@qq.com>
2023-05-02 13:05:41 +0800

    sd

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -14,7 +14,7 @@ def ridge(data):
     # return weight @ data
     x, y = read_data()
     i = np.eye(6)
-    h = 10000000
+    h = 0.05
     print(h)
     w = np.dot(np.linalg.inv(np.dot(x.T, x) + h * i), np.dot(x.T, y))
     m = np.dot(x.T, y) - np.dot(x.T, x)
